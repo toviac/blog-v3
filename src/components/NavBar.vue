9a516d750d788e937370ab2a79f1ae4eaa46f9ca
coolucifer <1045606768@qq.com>
2018-08-08 07:36:29 +0800

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,66 @@
+<!-- nav-bar -->
+<template>
+  <el-menu
+    class="nav-bar"
+    :router="true"
+    :default-active="activeIndex"
+    mode="horizontal"
+    @select="handleSelect"
+  >
+    <el-menu-item
+      v-for="item in navList"
+      :key="item.index"
+      :index="item.index"
+      :route="item.route"
+    >
+      {{ item.name }}
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: 'first',
+      navList: [
+        {
+          index: 'first',
+          name: '首页',
+          route: {
+            path: '/',
+          },
+        },
+        {
+          index: 'second',
+          name: 'Blogs',
+          route: {
+            path: '/',
+          },
+        },
+        {
+          index: 'thrid',
+          name: '小练习',
+          route: {
+            path: '/',
+          },
+        },
+      ],
+    };
+  },
+  components: {},
+  computed: {},
+  mounted() {},
+  methods: {
+    handleSelect(key) {
+      console.log('key: ', key);
+    },
+  },
+};
+
+</script>
+<style lang='scss' scoped>
+.nav-bar {
+  margin-top: calc(100% - 61px);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<!-- index -->
+<template>
+  <div class="home">
+    <nav-bar></nav-bar>
+    Hello World!
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+    NavBar,
+  },
+  computed: {},
+  mounted() {},
+  methods: {},
+};
+
+</script>
+<style lang='scss' scoped>
+</style>
