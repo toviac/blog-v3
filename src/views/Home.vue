3b1954cf6b9b71266d1e0e6952a6a60b77a2f235
coolucifer <1045606768@qq.com>
2018-08-10 23:50:48 +0800

    添加滚动到顶部按钮

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
-    <home-cover :ifShow.sync="showCover"></home-cover>
+    <!-- 背景图片 -->
+    <div class="home-background" :style="bgImgStyle"></div>
+    <!-- 灰色遮罩部分 -->
+    <el-collapse-transition>
+      <div v-show="showCover" class="home-cover" @mousewheel="onMouseWheel">
+          <el-button type="text" @click="onMouseWheel">
+            <i class="iconfont icon-arrowdown"></i>
+          </el-button>
+      </div>
+    </el-collapse-transition>
+    <!-- 主页面 -->
     <el-collapse-transition>
       <main-container v-show="!showCover" @toggle-show="showCover = true"></main-container>
     </el-collapse-transition>
@@ -8,21 +18,28 @@
 </template>
 
 <script>
-import HomeCover from '@/components/HomeCover.vue';
 import MainContainer from './MainContainer.vue';
 
 export default {
-  name: '',
+  name: 'Home',
   data() {
     return {
       showCover: true,
+      bgImgStyle: {
+        'background-image': 'url(69646886_p0.png)',
+      },
     };
   },
   components: {
-    HomeCover,
     MainContainer,
   },
   methods: {
+    onMouseWheel(e) {
+      // 鼠标滚轮向下滚动
+      if (!e.wheelDelta || e.wheelDelta < 0) {
+        this.showCover = false;
+      }
+    },
   },
   watch: {},
   mounted() {
@@ -31,4 +48,34 @@ export default {
 </script>
 
 <style lang="scss">
+.home {
+  .home-background {
+    z-index: -1;
+    position: fixed;
+    min-width: 800px;
+    width: 100vw;
+    height: 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: 50%;
+  }
+  .home-cover {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    height: 100vh;
+    padding-bottom: 20px;
+    background-color: rgba(0, 0, 0, .5);
+    .el-button--text {
+      padding: 0;
+      color: #e4e4e4;
+      &:hover {
+        color: #ffffff;
+      }
+    }
+    .iconfont {
+      font-size: 50px;
+    }
+  }
+}
 </style>
--- src/views/MainContainer.vue
+++ src/views/MainContainer.vue
@@ -2,13 +2,25 @@
 <template>
   <div class="main-container">
     <nav-bar></nav-bar>
-    <el-scrollbar ref="scrollbar" class="scroll-bar" wrap-style="overflow-x: hidden;">
+    <el-scrollbar id="scroll-box" ref="scrollbar"
+     class="scroll-bar" wrap-style="overflow-x: hidden;" @scroll.native="handleScroll">
       <el-row :gutter="20">
         <el-col :span="15">
             <content-wrapper></content-wrapper>
         </el-col>
         <el-col :span="5" id="side-bar-outer">
           <side-bar @toggle-show="handleToggleShow"></side-bar>
+          <!-- 滚动到顶部按钮 -->
+          <transition name="el-fade-in">
+            <el-button
+              plain
+              class="scroll-to-top-btn"
+              v-show="showScrollTop"
+              @click="scrollToTop"
+            >
+              <i class="el-icon-caret-top"></i>
+            </el-button>
+          </transition>
         </el-col>
       </el-row>
     </el-scrollbar>
@@ -24,6 +36,7 @@ export default {
   name: 'MainContainer',
   data() {
     return {
+      showScrollTop: true,
     };
   },
   components: {
@@ -32,17 +45,40 @@ export default {
     ContentWrapper,
   },
   computed: {},
-  mounted() {},
+  mounted() {
+    this.$nextTick(() => {
+      const el = this.$refs.scrollbar.$refs.wrap;
+      const ele = document.getElementById('scroll-box');
+      console.log('el: ', el);
+      console.log('ele: ', ele);
+      ele.addEventListener('scroll', this.handleScroll());
+    });
+  },
   methods: {
     handleToggleShow() {
-      // this.$emit('toggle-show');
-      this.scrollToTop();
+      this.$emit('toggle-show');
+      // this.scrollToTop();
+    },
+    handleScroll() {
+      console.log('listened');
+      // this.showScrollTop = this.$refs.scrollbar.$refs.wrap.scrollTop > 100;
     },
     scrollToTop() {
-      let el = this.$refs['scrollbar'].$refs['wrap'];
-      this.$nextTick(() => {
-        el.scrollTop = 0;
+      const el = this.$refs.scrollbar.$refs.wrap;
+      let timer = null;
+      cancelAnimationFrame(timer);
+      timer = requestAnimationFrame(function fn() {
+        if (el.scrollTop > 0) {
+          el.scrollTop -= 20;
+          timer = requestAnimationFrame(fn);
+        } else {
+          cancelAnimationFrame(timer);
+        }
       });
+      // this.$nextTick(() => {
+
+      //   el.scrollTop = 0;
+      // });
     },
   },
 };
@@ -66,6 +102,10 @@ export default {
   .scroll-wrapper {
     height: 300px;
     overflow-x: hidden !important;
+  }
+  .scroll-to-top-btn {
+    position: fixed;
+    bottom: 300px;
   }
 }
 </style>
