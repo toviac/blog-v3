ff510bac7c002160353c3f20a5612a3acfd249e7
coolucifer <1045606768@qq.com>
2018-08-14 07:58:51 +0800

    添加三篇日志

--- src/views/layout/ContentWrapper.vue
+++ src/views/layout/ContentWrapper.vue
@@ -1,7 +1,7 @@
 <!-- 主要内容 -->
 <template>
   <div class="content-wrapper">
-    <md-editor></md-editor>
+    <!-- <md-editor></md-editor> -->
     <div v-html="markdownHTML"></div>
   </div>
 </template>
@@ -10,21 +10,25 @@
 import marked from '@/utils/marked';
 import MdEditor from '@/components/MdEditor.vue';
 // 引入md
-import _20171121 from '@/sources/2017-11-21.md';
+import Sources from '@/sources';
 
 export default {
   name: 'ContentWrapper',
+  props : {
+    mdTitle: {
+      type: String,
+      default: '',
+    },
+  },
   data() {
-    return {
-      value: '',
-    };
+    return {};
   },
   components: {
     MdEditor,
   },
   computed: {
     markdownHTML() {
-      return marked(_20171121);
+      return marked(Sources._20171206);
     },
   },
   mounted() {},
@@ -37,10 +41,13 @@ export default {
   width: 100%;
   padding: 10px;
   margin-bottom: 10px;
-  // min-height: calc(100vh - 81px);
-  min-height: 3000px;
+  min-height: calc(100vh - 81px);
   background-color: #fff;
   border-radius: 4px;
   box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+  // 代码样式
+  code {
+    font-family: 'Consolas';
+  }
 }
 </style>
