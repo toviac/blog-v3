e26d5573c527aae469ea8dd10fe2101c3e64e0d1
coolucifer <1045606768@qq.com>
2018-08-23 08:22:54 +0800

    hacker news 例子

--- server/config/config.default.js
+++ server/config/config.default.js
@@ -1,1 +1,13 @@
 exports.keys  = '334433';
+// 添加view配置
+exports.view = {
+  defaultViewEngine: 'nunjucks',
+  mapping: {
+    '.tpl': 'nunjucks',
+  },
+};
+
+exports.news = {
+  pageSize: 5,
+  serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
