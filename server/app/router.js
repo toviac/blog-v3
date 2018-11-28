37897f0745474071c0cb71236987ec8d452514bc
coolucifer <1045606768@qq.com>
2018-11-28 20:49:12 +0800

    分离nodejs后端

--- server/app/router.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-  const { router, controller } = app;
-  // router.get('/', controller.home.index);
-  router.get('/api/filelist', controller.fs.list);
-  router.get('/api/file', controller.fs.content);
-  // router.get('/news', controller.news.list);
-};
