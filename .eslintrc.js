6b7cc97b2f3eb4df6020d02ea7b318c6ce470cc5
coolucifer <1045606768@qq.com>
2018-11-28 20:46:44 +0800

    更新eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,25 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
-    '@vue/airbnb'
+    '@vue/airbnb',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-plusplus': [
+      process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      { allowForLoopAfterthoughts: true },
+    ],
+    'arrow-parens': [
+      process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'as-needed',
+    ],
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    parser: 'babel-eslint',
+  },
+};
