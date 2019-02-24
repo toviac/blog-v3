d2961067c22e8255b9b9e51998cd6640bfe3906c
victor <1045606768@qq.com>
2019-02-24 20:35:15 +0800

    引入socket.io

--- src/main.js
+++ src/main.js
@@ -4,10 +4,26 @@ import 'element-ui/lib/theme-chalk/index.css';
 import 'mavon-editor/dist/css/index.css';
 import mavonEditor from 'mavon-editor';
 // import highlightcode from './highlightcode';
+
+// 引入socket.io
+import VueSocketio from 'vue-socket.io';
+
 import App from './App.vue';
 import router from './routes';
 import store from './store';
 import './registerServiceWorker';
+
+// socket.io 引入
+Vue.use(new VueSocketio({
+  debug: true,
+  connection: 'http://127.0.0.1:7001',
+  vuex: {
+    store,
+    // Prefix for emitting server side vuex actions
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_',
+  },
+}));
 
 Vue.config.productionTip = false;
 
