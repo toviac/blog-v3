9bcb3697117d3cf46d023ebf834a9be4481bedd4
victor <1045606768@qq.com>
2019-02-24 20:36:25 +0800

    测试页面

---
+++ src/tests/CardBtn.vue
@@ -0,0 +1,44 @@
+<!-- card-btn -->
+<template>
+  <div class="card-btn">
+    <div class="btn-list">
+      <el-card class="card" v-for="item in cardList" :key="item.name">
+        {{ item.name }}
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cardList: [
+        { name: 'first' },
+        { name: 'second' },
+        { name: 'third' },
+        { name: 'fourth' },
+        { name: 'fifth' },
+      ],
+    };
+  },
+  components: {},
+  computed: {},
+  mounted() {},
+  methods: {},
+};
+
+</script>
+<style lang="scss">
+.card-btn {
+  padding: 10px;
+  background-color: #ccc;
+  width: 1000px;
+  height: 1000px;
+  .card {
+    cursor: pointer;
+    width: 200px;
+    height: 124px;
+  }
+}
+</style>
